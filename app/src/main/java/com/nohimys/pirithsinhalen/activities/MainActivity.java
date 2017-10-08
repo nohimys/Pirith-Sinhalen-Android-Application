fe2cfd2408b4768abeff8408b25c2502dea66021
Nohim Sandeepa <itsnohim@gmail.com>
2017-10-08 23:42:21 +0530

    Added packages and moved the MainActivity into a package.

--- app/src/main/java/com/nohimys/pirithsinhalen/activities/MainActivity.java
+++ app/src/main/java/com/nohimys/pirithsinhalen/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.nohimys.pirithsinhalen;
+package com.nohimys.pirithsinhalen.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nohimys.pirithsinhalen.R;
 
 public class MainActivity extends AppCompatActivity {
 
